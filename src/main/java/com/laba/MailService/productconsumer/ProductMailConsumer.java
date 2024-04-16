ced170b645c50a9be26ecb9cdc70a71cc091ab8b
nasan <nesrin.asan@hepsiburada.com>
2024-04-16 16:34:46 +0300

    ders 17

---
+++ src/main/java/com/laba/MailService/productconsumer/ProductMailConsumer.java
@@ -0,0 +1,39 @@
+package com.laba.MailService.productconsumer;
+
+import org.springframework.kafka.annotation.DltHandler;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductMailConsumer {
+
+    private final KafkaMailFailService kafkaMailFailService;
+
+    public ProductMailConsumer(KafkaMailFailService kafkaMailFailService) {
+        this.kafkaMailFailService = kafkaMailFailService;
+    }
+
+
+    @KafkaListener(topics = "${topic.mail}", groupId = "mailproduct")
+    @RetryableTopic(
+            attempts = "2",
+            dltStrategy = DltStrategy.FAIL_ON_ERROR)
+    public void listen(String message) {
+
+        String s = null;
+        s.equals("");
+        System.out.println(message);
+
+    }
+
+
+    @DltHandler
+    public void handleDltPayment(@Header(KafkaHeaders.RECEIVED_TOPIC) String topic) {
+        System.out.println();
+
+    }
+}
